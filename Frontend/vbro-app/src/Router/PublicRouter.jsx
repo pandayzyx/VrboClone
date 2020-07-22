b3ac14f4bfa1f9cf56a0ffb2ecec0c96d22efaa6
pandayzyx <gauravx.gp@gmail.com>
2020-07-22 23:17:38 +0530

    CrouselBug

--- Frontend/vbro-app/src/Router/PublicRouter.jsx
+++ Frontend/vbro-app/src/Router/PublicRouter.jsx
@@ -1,21 +1,22 @@
 import React from "react"
 import {Switch,Route} from "react-router-dom"
-import OwnerHelp from  "./RouterComponents/NavbarRoutes/OwnerHelp"
-import TravellerHelp from  "./RouterComponents/NavbarRoutes/TravelerHelp"
-import TrustAndSafety from  "./RouterComponents/NavbarRoutes/TrustAndSafety"
-import PropertyManagerHelp from  "./RouterComponents/NavbarRoutes/PropertyManagerHelp"
-import ListYourProperty from  "./RouterComponents/NavbarRoutes/ListYourProperty"
+import Home from  "./RouterComponents/Home"
+// import OwnerHelp from  "./RouterComponents/NavbarRoutes/OwnerHelp"
+// import TravellerHelp from  "./RouterComponents/NavbarRoutes/TravelerHelp"
+// import TrustAndSafety from  "./RouterComponents/NavbarRoutes/TrustAndSafety"
+// import PropertyManagerHelp from  "./RouterComponents/NavbarRoutes/PropertyManagerHelp"
+// import ListYourProperty from  "./RouterComponents/NavbarRoutes/ListYourProperty"
 
 
 function PublicRouter(){
     return(
         <Switch>
-        <Route path = "/"></Route>
-        <Route path = "/travellerhelp" render = {()=><TravellerHelp/>}  ></Route>
+        <Route exact path = "/" render = {()=><Home/>} ></Route>
+        {/* <Route path = "/travellerhelp" render = {()=><TravellerHelp/>}  ></Route>
         <Route path = "/ownerhelp" render = {()=> <OwnerHelp/>}  ></Route>
         <Route path = "/propertymanager" render = {()=><PropertyManagerHelp/> } ></Route>
         <Route path = "/trustandsafety" render = {()=><TrustAndSafety/>}  ></Route>
-        <Route path = "/listyourproperty" render = {()=><ListYourProperty/>}  ></Route>
+        <Route path = "/listyourproperty" render = {()=><ListYourProperty/>}  ></Route> */}
         </Switch>
     )
 }
