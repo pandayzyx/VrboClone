0ec16c4d091f4c0bc10106ef6a129db536b9c38d
pandayzyx <gauravx.gp@gmail.com>
2020-08-09 21:22:34 +0530

    LastUpdate

--- Frontend/vbro-app/src/App.js
+++ Frontend/vbro-app/src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./Components/CommonComponents/Navbar/Navbar.jsx"
 import Footer from "./Components/CommonComponents/Footer/Footer.jsx"
 import PublicRouter from "./Router/PublicRouter"
 
+
+
 function App() {
   return (
     <div className="App">
