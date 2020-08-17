0e48189e4a0a25ac7e604781a52969461afa69c8
Dev Ganesh <devganesh91@gmail.com>
2020-08-17 18:34:43 +0530

    persisted state

--- Frontend/vbro-app/src/Components/CommonComponents/Pagination/Pagination.jsx
+++ Frontend/vbro-app/src/Components/CommonComponents/Pagination/Pagination.jsx
@@ -7,147 +7,147 @@ const queryString = require("query-string");
 
 var windowurl = "";
 class Pagination extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			curr_page: 1,
-			total_page: 10,
-			params: "",
-			windowurl: "",
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      curr_page: 1,
+      total_page: 10,
+      params: "",
+      windowurl: "",
+    };
+  }
 
-	componentDidMount() {
-		let { search } = this.props;
-		const values = queryString.parse(search);
-		let params = values;
-		if (Object.keys(params).length !== 0) {
-			for (let key in params) {
-				if (key === "pageNum") {
-					console.log(params[key]);
-					this.setState({
-						curr_page: Number(params[key]) || 1,
-					});
-				}
-			}
-			this.setState({
-				params: params,
-			});
-		}
-	}
+  componentDidMount() {
+    let { search } = this.props;
+    const values = queryString.parse(search);
+    let params = values;
+    if (Object.keys(params).length !== 0) {
+      for (let key in params) {
+        if (key === "pageNum") {
+          console.log(params[key]);
+          this.setState({
+            curr_page: Number(params[key]) || 1,
+          });
+        }
+      }
+      this.setState({
+        params: params,
+      });
+    }
+  }
 
-	handleClick = (id) => {
-		let { getListData, history } = this.props;
-		let { params } = this.state;
-		console.log(params);
-		console.log(Object.keys(params).length);
-		var tabUrl = "";
-		if (params !== "" && Object.keys(params).length !== 0) {
-			for (let key in params) {
-				if (key !== "pageNum") {
-					console.log("ua re in params");
-					tabUrl = tabUrl + key + "=" + params[key] + "&";
-				}
-			}
-		} else {
-			console.log("else");
-			params = { pageNum: id };
-			tabUrl = "";
-		}
+  handleClick = (id) => {
+    let { getListData, history } = this.props;
+    let { params } = this.state;
+    console.log(params);
+    console.log(Object.keys(params).length);
+    var tabUrl = "";
+    if (params !== "" && Object.keys(params).length !== 0) {
+      for (let key in params) {
+        if (key !== "pageNum") {
+          console.log("ua re in params");
+          tabUrl = tabUrl + key + "=" + params[key] + "&";
+        }
+      }
+    } else {
+      console.log("else");
+      params = { pageNum: id };
+      tabUrl = "";
+    }
 
-		history.push(`?${tabUrl}pageNum=${id}`);
-		this.setState({
-			curr_page: id,
-			windowurl: tabUrl,
-		});
-		params["pageNum"] = id;
-		const { handlePagination } = this.props;
-		const url = "http://3.134.153.158:80/properties";
-		getListData({
-			url: url,
-			params: params,
-		});
-		handlePagination(id);
-	};
+    history.push(`?${tabUrl}pageNum=${id}`);
+    this.setState({
+      curr_page: id,
+      windowurl: tabUrl,
+    });
+    params["pageNum"] = id;
+    const { handlePagination } = this.props;
+    const url = `${process.env.REACT_APP_PROPERTY_HOST}/properties`;
+    getListData({
+      url: url,
+      params: params,
+    });
+    handlePagination(id);
+  };
 
-	render() {
-		let { curr_page } = this.state;
-		let { totalResults } = this.props;
-		console.log(totalResults);
-		var total_page = Math.ceil(totalResults / 20);
-		var arr = [];
-		for (let i = 1; i <= total_page; i++) {
-			arr.push(i);
-		}
+  render() {
+    let { curr_page } = this.state;
+    let { totalResults } = this.props;
+    console.log(totalResults);
+    var total_page = Math.ceil(totalResults / 20);
+    var arr = [];
+    for (let i = 1; i <= total_page; i++) {
+      arr.push(i);
+    }
 
-		return (
-			<div>
-				<nav aria-label="...">
-					<ul className="pagination">
-						<li className="page-item">
-							{/* <Link
+    return (
+      <div>
+        <nav aria-label="...">
+          <ul className="pagination">
+            <li className="page-item">
+              {/* <Link
 								to={`?${windowurl}pageNum=${curr_page > 1 ? curr_page - 1 : 1}`}
 							> */}
-							<button
-								className="page-link"
-								onClick={() =>
-									this.handleClick(curr_page > 1 ? curr_page - 1 : 1)
-								}
-							>
-								Prev
-							</button>
-							{/* </Link> */}
-						</li>
-						{arr &&
-							arr.map((item) => (
-								<li
-									key={uuidv4()}
-									className={
-										curr_page === item ? "page-item active" : "page-item"
-									}
-								>
-									<button
-										className="page-link"
-										onClick={() => this.handleClick(item)}
-									>
-										{item}
-									</button>
-								</li>
-							))}
-						<li className="page-item">
-							{/* <Link
+              <button
+                className="page-link"
+                onClick={() =>
+                  this.handleClick(curr_page > 1 ? curr_page - 1 : 1)
+                }
+              >
+                Prev
+              </button>
+              {/* </Link> */}
+            </li>
+            {arr &&
+              arr.map((item) => (
+                <li
+                  key={uuidv4()}
+                  className={
+                    curr_page === item ? "page-item active" : "page-item"
+                  }
+                >
+                  <button
+                    className="page-link"
+                    onClick={() => this.handleClick(item)}
+                  >
+                    {item}
+                  </button>
+                </li>
+              ))}
+            <li className="page-item">
+              {/* <Link
 								to={`?${windowurl}pageNum=${
 									curr_page < total_page ? curr_page + 1 : total_page
 								}`}
 							> */}
-							<button
-								className="page-link"
-								onClick={() =>
-									this.handleClick(
-										curr_page < total_page ? curr_page + 1 : total_page
-									)
-								}
-							>
-								Next
-							</button>
-							{/* </Link> */}
-						</li>
-					</ul>
-				</nav>
-			</div>
-		);
-	}
+              <button
+                className="page-link"
+                onClick={() =>
+                  this.handleClick(
+                    curr_page < total_page ? curr_page + 1 : total_page
+                  )
+                }
+              >
+                Next
+              </button>
+              {/* </Link> */}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 const MapStateToProps = (state) => {
-	return {
-		dataListingPage: state.list.dataListingPage,
-		totalResults: state.list.totalResults,
-	};
+  return {
+    dataListingPage: state.list.dataListingPage,
+    totalResults: state.list.totalResults,
+  };
 };
 const MapDisaptchToProps = (dispatch) => {
-	return {
-		getListData: (payload) => dispatch(getListData(payload)),
-	};
+  return {
+    getListData: (payload) => dispatch(getListData(payload)),
+  };
 };
 export default connect(MapStateToProps, MapDisaptchToProps)(Pagination);
--- Frontend/vbro-app/src/Redux/store.js
+++ Frontend/vbro-app/src/Redux/store.js
@@ -1,23 +1,51 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import {registerreducer} from "../Redux/RegisterUser/registerreducer"
-import {loginreducer} from "../Redux/LoginUser/loginreducer"
-import {listingreducer} from  "../Redux/Listing/listingreducer"
-import {entityreducer} from "../Redux/Entity/entityreducer"
-import {bookingreducer} from "../Redux/Booking/bookingreducer"
+import { registerreducer } from "../Redux/RegisterUser/registerreducer";
+import { loginreducer } from "../Redux/LoginUser/loginreducer";
+import { listingreducer } from "../Redux/Listing/listingreducer";
+import { entityreducer } from "../Redux/Entity/entityreducer";
+import { bookingreducer } from "../Redux/Booking/bookingreducer";
 // import thunk from "redux-thunk"
 
-const thunk = store => (next) => (action) => {
-	typeof action === "function"?action(store.dispatch):next(action)
+const thunk = (store) => (next) => (action) => {
+  typeof action === "function" ? action(store.dispatch) : next(action);
 };
-const rootreducer = combineReducers({register:registerreducer,login:loginreducer,list:listingreducer,entity:entityreducer,book:bookingreducer});
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("vrboState", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("vrboState");
+    if (serializedState == null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+const rootreducer = combineReducers({
+  register: registerreducer,
+  login: loginreducer,
+  list: listingreducer,
+  entity: entityreducer,
+  book: bookingreducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-	rootreducer,
-	composeEnhancers(applyMiddleware(thunk))
+  rootreducer,
+  loadFromLocalStorage(),
+  composeEnhancers(applyMiddleware(thunk))
 );
-console.log(store.getState())
 
+store.subscribe(()=> saveToLocalStorage(store.getState()))
 
-
+console.log(store.getState());
